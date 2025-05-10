93a41f159c3b0f5311e279b4db07ea2b8588a101
Santillan-C <santillango10405@gmail.com>
2025-05-10 06:32:23 -0700

    yeah, again

--- helper/youtube_scraper.js
+++ helper/youtube_scraper.js
@@ -25,7 +25,8 @@ const fetchData = async function fetchData(url) {
   const { data } = await axios.get(url, {
     headers: {
       "User-Agent":
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+        "(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
     },
   });
   return data;
