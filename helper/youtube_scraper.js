966ed92b590d82b4d859f79edb2fe5e750000b5c
Santillan-C <santillango10405@gmail.com>
2025-05-10 06:17:42 -0700

    I changed the header again

--- helper/youtube_scraper.js
+++ helper/youtube_scraper.js
@@ -15,6 +15,8 @@ const userAgents = [
 /*
 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36
 Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36
+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 " +
+        "(KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
 */
 
 const fetchData = async function fetchData(url) {
@@ -22,7 +24,8 @@ const fetchData = async function fetchData(url) {
     userAgents[Math.floor(Math.random() * userAgents.length)];
   const { data } = await axios.get(url, {
     headers: {
-      "User-Agent": userAgents[1],
+      "User-Agent":
+        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
     },
   });
   return data;
